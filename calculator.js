7bb0a910adf29313a1e6d9d6e1d22b8d7c4bab3c
Bernardo Villanova de Santana <73114481+Briviet@users.noreply.github.com>
2020-10-20 10:29:23 -0300

    Create calculator.js
    Simple javascript for calculator operation

---
+++ calculator.js
@@ -0,0 +1,25 @@
+function clicar(){    
+        var num1 = document.getElementById("n1")
+        var num2 = document.getElementById("n2")
+        var op = document.getElementById("operacoes").value
+
+        var n1 = Number(num1.value)
+        var n2 = Number(num2.value)
+        var plus = n1 + n2
+        var less = n1-n2
+        var mul = n1*n2
+        var di = n1/n2
+        var ex = n1^n1
+        
+
+        if (op == "+")
+            res.innerHTML = `${n1} and ${n2} will have as a result ${plus}`
+            if (op == "-")
+                    res.innerHTML = `${n1} and ${n2} will have as a result ${less}`
+            if (op == "^")
+                    res.innerHTML = `${n1} and ${n2} will have as a result ${ex}`
+            if (op == "/")
+                    res.innerHTML = `${n1} and ${n2} will have as a result ${di}`
+            if (op == "*")
+                    res.innerHTML = `${n1} and ${n2} will have as a result ${ex}`
+}
